98648d0b9aa1b76d9dd03b3f1857e3ed1ee32e53
Liam Dawson <liam@liamdawson.me>
2016-03-25 21:28:18 +1000

    Basic Jade support

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const Koa = require('koa'),
+      Router = require('koa-router'),
+      KoaJade = require('koa-jade');
+
+var app = new Koa();
+var jadeware = new KoaJade({
+  viewPath: "./views",
+  debug: true,
+  pretty: true,
+  app: app
+});
+
+app.use(new Router().get('/', function*() {
+  this.render('index');
+}).middleware());
+
+app.listen(3000);
