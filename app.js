0fca836ac786a7f2474cc0fd108c2ac870446bd9
Liam Dawson <liam@liamdawson.me>
2016-03-25 22:04:40 +1000

    Index page with mattered games shown
    * Add game matter for a few games
    * Load game matter in app
    * Render index page with checkboxes for each game

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const Koa = require('koa'),
       Router = require('koa-router'),
-      KoaJade = require('koa-jade');
+      KoaJade = require('koa-jade'),
+      fs = require('fs-promise'),
+      path = require('path'),
+      gameMatterDir = "game_matter";
 
 var app = new Koa();
 var jadeware = new KoaJade({
   viewPath: "./views",
   debug: true,
   pretty: true,
+  noCache: true,
   app: app
 });
 
-app.use(new Router().get('/', function*() {
-  this.render('index');
-}).middleware());
+fs.readdir(gameMatterDir).then((files) => files.map((file) => fs.readJson(path.join(gameMatterDir, file)).then((obj) => obj)), (err) => {
+  console.error(`Couldn't load game matter files:\n${err.message}`);
+  process.exit(1);
+}).then((gamePromises) => {
+  Promise.all(gamePromises).then((games) => {
+    runServer(games);
+  }, (err) => {
+    console.error(`Couldn't load a game matter file:\n${err.message}`);
+    process.exit(2);
+  });
+});
 
-app.listen(3000);
+function runServer(games) {
+  jadeware.locals.games = games;
+
+  app.use(new Router().get('/', function*() {
+    this.render('index');
+  }).middleware());
+
+  app.listen(3000);
+}
