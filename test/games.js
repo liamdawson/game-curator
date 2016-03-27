9518d68a9564e95c2326028f976bd4c87c92b1ce
Liam Dawson <liam@liamdawson.me>
2016-03-27 12:07:53 +1000

    Add test for game matter schema validation

--- test/games.js
+++ test/games.js
@@ -1,80 +1,41 @@
-'use strict'
+'use strict';
 const fs = require('fs-promise')
     , path = require('path')
-    , gameMatterDir = '../games'
+    , gameMatterDir = path.resolve(__dirname, '../games')
     , expect = require('expect.js')
+    , isMyJsonValid = require('is-my-json-valid')
+    , jsonSchema = require('../schemas/game/1.json')
+    , validate = isMyJsonValid(jsonSchema)
 
 function getFiles(){
   return fs
-    .readdir(gameMatterDir) 
-    .then(files => (
-      files
-        .map(file => fs.readJson(path.resolve(gameMatterDir, file)))
-        .then(gamePromises => Promise.all(gamePromises))
-    ))
+    .readdir(gameMatterDir)
+    .then(files => Promise.all(files.map(file => fs.readJson(path.resolve(gameMatterDir, file)))))
 }
 
-describe('Games folder and games', function(){
-  
+describe('Games folder', function(){
+
   it('should be a directory', function(){
     fs
-      .readdir(gameMatterDir) 
+      .readdir(gameMatterDir)
       .then(files => (
         expect(files).to.be.an('array')
       ))
   })
-  
-  it('should have a name', function(){
-    getFiles()
-    .then(games => {
-      games.forEach(game => {
-          expect(game).to.have.property('name')
-          expect(game.name).to.be.an('string')
-        })
-    })
+
+})
+
+describe('Games', function() {
+
+  it('should all be valid', function() {
+    return getFiles().then((games) => games.forEach(gameObj => {
+      if(validate(gameObj)) {
+        return true;
+      } else {
+        let message = `Validation of game schema failed:\n${validate.errors.map(e => `${e.field} ${e.message}`).join("\n")}\n\nFor:\n${JSON.stringify(gameObj)}`
+        throw(new Error(message));
+      }
+    }));
   })
-  
-  it('should have a time', function(){
-    getFiles()
-    .then(games =>  games.forEach(game => {
-      expect(game).to.have.property('time')
-      expect(game.time).to.be.an('string')
-    }))
-  })
-  
-  it('should have players count', function(){
-    getFiles()
-    .then(games =>  games.forEach(game => {
-      expect(game).to.have.property('players')
-      
-      expect(game.players).to.have.property('ideal')
-      expect(game.players.ideal).to.be.an('array')
-      
-      expect(game.players).to.have.property('possible')
-      expect(game.players.possible).to.be.an('array')
-    }))
-  })
-  
-  it('should have complexity', function(){
-    getFiles()
-    .then(games =>  games.forEach(game => {
-      expect(game).to.have.property('complexity')
-      
-      expect(game.complexity).to.have.property('rules')
-      expect(game.complexity.rules).to.be.an('string')
-      
-      expect(game.complexity).to.have.property('meta')
-      expect(game.complexity.meta).to.be.an('string')
-    }))
-  })
-  
-  it('should have an overview', function(){
-    getFiles()
-    .then(games =>  games.forEach(game => {
-      expect(game).to.have.property('overview')
-      
-      expect(game.overview).to.have.property('basic')
-      expect(game.overview.basic).to.be.an('string')
-    }))
-  })
+
 })
