601714103017937c5b1bf2e36408fbca523295ba
Liam Dawson <liam@ldaws.com>
2018-06-21 00:17:15 +1000

    test(storybook): add useful storybook plugins

--- stories/misc.stories.tsx
+++ stories/misc.stories.tsx
@@ -1,3 +1,4 @@
+import { boolean, withKnobs } from '@storybook/addon-knobs/react';
 import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
@@ -6,4 +7,5 @@ import NewVersionBanner from '../src/components/misc/NewVersionBanner'
 import '../src/index.css';
 
 storiesOf('Miscellaneous Components', module)
-  .add('new version banner', () => <NewVersionBanner display={true} />);
+  .addDecorator(withKnobs)
+  .add('new version banner', () => <NewVersionBanner display={boolean('Display', true)} />);
